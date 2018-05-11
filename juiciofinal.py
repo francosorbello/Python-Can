30767282e254950a4e6458edc035bbedc3b8c5ba
Rulo <franco.sorbelo@gmail.com>
2018-05-11 16:19:38 -0300

    Update files

--- juiciofinal.py
+++ juiciofinal.py
@@ -1,16 +1,22 @@
 from algo1 import *
 from random import randint
 
+def busco_ocupadas(x,i):
+    for a in range(0,len(x)):
+        if x[a][i]==1:
+            return False
+    return True
+
 def asigno_city(b,x,n):
     for i in range(0,n):
         costo=b[i][0] #asigno un costo base
         aux=0 #esta var controla en caso de que la primer ciudad sea la de menor costo
+        ciudad_ocupada=busco_ocupadas(x,i)
         for j in range(0,n):
-            if b[i][j]<costo and x[i][j]==0:
+            if b[i][j]<costo and x[i][j]==0 and ciudad_ocupada==True:
                 aux=1#si ya tengo una sol cambio el valor de aux para evitar usar la primer ciudad
                 x[i][j]=1 #pongo un terminator en esa ciudad
-                #costo=b[i][j+1]
-                break #ACA TENGO QUE TERMINAR O SIGO BUSCANDO UNO MEJOR? SI SIGO BUSCANDO SIGUE SIENDO GREEDY?
+                break 
             if j==(n-1) and aux==0:
                 x[i][0]=1  
     return x
